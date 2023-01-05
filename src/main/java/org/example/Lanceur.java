74dd0d61c53a24b73cb734c6381435fd0edad20a
RafaelVMoraes <80759332+RafaelVMoraes@users.noreply.github.com>
2023-01-05 08:53:31 +0100

    add dames files

---
+++ src/main/java/org/example/Lanceur.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import javax.swing.*;
+
+
+public class Lanceur {
+
+	
+	public static void main(String[] args) {
+		try{
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+		}
+		catch(Exception e){}
+		JFrame f = new JFrame();
+		f.setSize(600, 600);
+		f.setLocationRelativeTo(null);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.add(new Plateau(9));
+		f.setVisible(true);
+
+	}
+
+}
