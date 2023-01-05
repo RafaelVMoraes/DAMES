5021d731516f74918c61789dfb9201f20a109bb1
RafaelVMoraes <80759332+RafaelVMoraes@users.noreply.github.com>
2023-01-05 09:01:59 +0100

    add tests to case

---
+++ src/test/java/org/example/CaseTest.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CaseTest {
+
+    @Test
+    void getCouleur() {
+    }
+
+    @Test
+    void isSelectionnee() {
+    }
+
+    @Test
+    void setSelectionnee() {
+    }
+
+    @Test
+    void paintComponent() {
+    }
+}
